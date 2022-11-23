d9fe86ec059eae6e7e29dd91c56700e35557d3b5
ankita <90205130+Ankita-Manmoandas@users.noreply.github.com>
2022-11-23 17:34:02 +0000

    added geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
 import './App.scss';
+import React from "react";
+import { useGeolocated } from "react-geolocated";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1> Today's weather based on your location</h1>
-     
-      </header>
-    </div>
-  );
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+  useGeolocated({
+      positionOptions: {
+          enableHighAccuracy: false,
+      },
+      userDecisionTimeout: 5000,
+  });
+
+  return !isGeolocationAvailable ? (
+        <div>Your browser does not support Geolocation</div>
+    ) : !isGeolocationEnabled ? (
+        <div>Geolocation is not enabled</div>
+    ) : coords ? (
+        <table>
+            <tbody>
+                <tr>
+                    <td>latitude</td>
+                    <td>{coords.latitude}</td>
+                </tr>
+                <tr>
+                    <td>longitude</td>
+                    <td>{coords.longitude}</td>
+                </tr>
+                <tr>
+                    <td>altitude</td>
+                    <td>{coords.altitude}</td>
+                </tr>
+                <tr>
+                    <td>heading</td>
+                    <td>{coords.heading}</td>
+                </tr>
+                <tr>
+                    <td>speed</td>
+                    <td>{coords.speed}</td>
+                </tr>
+            </tbody>
+        </table>
+    ) : (
+        <div>Getting the location data&hellip; </div>
+    );
+
+
 }
+
 
 export default App;
