5e9cd57373098dbb9eae87d50dd3944a7f0aceaf
ankita <90205130+Ankita-Manmoandas@users.noreply.github.com>
2022-11-23 16:49:15 +0000

    cleaned up files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1> Today's weather based on your location</h1>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
